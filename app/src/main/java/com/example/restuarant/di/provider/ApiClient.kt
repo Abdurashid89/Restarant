6b44d26a455ad904eb7dc746bccbb3fdc9d9cddc
shohboz <shohbozqoraboyev5@gmail.com>
2021-01-11 16:20:35 +0500

    first commit

---
+++ app/src/main/java/com/example/restuarant/di/provider/ApiClient.kt
@@ -0,0 +1,27 @@
+package com.example.restuarant.di.provider
+
+import com.example.restuarant.di.ServerPath
+import com.example.restuarant.model.server.ResApi
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
+import javax.inject.Provider
+
+class ApiClient @Inject constructor(
+    private val okHttpClient: OkHttpClient,
+    @ServerPath private val serverPath: String
+) : Provider<ResApi> {
+
+    override fun get() = ApiWithChangesRegistration(getOriginalApi())
+
+    private fun getOriginalApi() =
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(okHttpClient)
+            .baseUrl(serverPath)
+            .build()
+            .create(ResApi::class.java)
+}
