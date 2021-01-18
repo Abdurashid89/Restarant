473823bc893eaf106285dccdca9721728d4734ef
acer <davronbekprogrammer@gmail.com>
2021-01-18 15:02:52 +0500

    ProductFragment.kt

--- app/src/main/java/com/example/restuarant/ui/wareHouse/product/ProductFragment.kt
+++ app/src/main/java/com/example/restuarant/ui/wareHouse/product/ProductFragment.kt
@@ -1,7 +1,20 @@
 package com.example.restuarant.ui.wareHouse.product
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import com.example.restuarant.R
+import com.example.restuarant.databinding.FragmentProductBinding
+import com.example.restuarant.ui.global.BaseFragment
 
-class ProductFragment : Fragment() {
+class ProductFragment() : BaseFragment() {
+    override val layoutRes: Int = R.layout.fragment_product
 
+    private lateinit var binding: FragmentProductBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentProductBinding.bind(view)
+
+        binding
+    }
 }
