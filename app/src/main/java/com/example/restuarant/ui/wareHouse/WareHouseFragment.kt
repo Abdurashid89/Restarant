c11544a147e2ce56e0dfcb03b1f4677733b19196
acer <davronbekprogrammer@gmail.com>
2021-01-21 18:46:21 +0500

    UploadImageContinue222222

--- app/src/main/java/com/example/restuarant/ui/wareHouse/WareHouseFragment.kt
+++ app/src/main/java/com/example/restuarant/ui/wareHouse/WareHouseFragment.kt
@@ -1,7 +1,11 @@
 package com.example.restuarant.ui.wareHouse
 
-import androidx.fragment.app.Fragment
+import com.example.restuarant.R
+import com.example.restuarant.databinding.FragmentWareHouseBinding
+import com.example.restuarant.ui.global.BaseFragment
 
-class WareHouseFragment : Fragment() {
+class WareHouseFragment() : BaseFragment() {
+    override val layoutRes: Int = R.layout.fragment_ware_house
+    private lateinit var binding: FragmentWareHouseBinding
 
 }
