11a422aac6c335ed88e86797e102a58644f10d4e
acer <davronbekprogrammer@gmail.com>
2021-01-26 13:44:59 +0500

    ware_house_add_product

---
+++ app/src/main/java/com/example/restuarant/model/interactor/WareHouseInteractor.kt
@@ -0,0 +1,29 @@
+package com.example.restuarant.model.interactor
+
+import com.example.restuarant.model.entities.MessageData
+import com.example.restuarant.model.entities.MessageDataWithoutMessageType
+import com.example.restuarant.model.entities.ProductData
+import com.example.restuarant.model.server.ResApi
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
+
+class WareHouseInteractor @Inject constructor(
+    private val api: ResApi
+) {
+    fun product(data: ProductData): Single<MessageData> {
+        return api.addProduct(data)
+            .map {
+                it
+            }.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    fun productExistOrNot(name: String): Single<MessageDataWithoutMessageType> {
+        return api.productExistOrNot(name).map {
+            it
+        }.subscribeOn(Schedulers.io())
+            .doOnError { }
+    }
+}
--- app/src/main/java/com/example/restuarant/ui/wareHouse/WareHouseFragment.kt
+++ app/src/main/java/com/example/restuarant/ui/wareHouse/WareHouseFragment.kt
@@ -1,11 +1,52 @@
 package com.example.restuarant.ui.wareHouse
 
+import android.os.Bundle
+import android.view.View
 import com.example.restuarant.R
 import com.example.restuarant.databinding.FragmentWareHouseBinding
+import com.example.restuarant.extentions.showSnackMessage
+import com.example.restuarant.presentation.were_house.WareHousePresenter
+import com.example.restuarant.presentation.were_house.WareHouseView
 import com.example.restuarant.ui.global.BaseFragment
+import moxy.presenter.InjectPresenter
+import moxy.presenter.ProvidePresenter
 
-class WareHouseFragment() : BaseFragment() {
+class WareHouseFragment() : BaseFragment(), WareHouseView {
     override val layoutRes: Int = R.layout.fragment_ware_house
     private lateinit var binding: FragmentWareHouseBinding
+
+    @InjectPresenter
+    lateinit var presenter: WareHousePresenter
+
+    @ProvidePresenter
+    fun providePresenter(): WareHousePresenter = scope.getInstance(WareHousePresenter::class.java)
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentWareHouseBinding.bind(view)
+
+
+
+        binding.btnLogOut.setOnClickListener {
+            presenter.onBackPressed()
+        }
+    }
+
+    override fun showMessage(message: String) {
+        showSnackMessage(message)
+    }
+
+    override fun makeLoadingVisible(status: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun openErrorDialog(message: String, status: Boolean) {
+
+    }
+
+    override fun onBackPressed() {
+        presenter.onBackPressed()
+    }
 
 }
