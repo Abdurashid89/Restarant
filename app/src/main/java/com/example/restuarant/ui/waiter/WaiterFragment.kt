496be0d7209d6bb2e172fa48b777ff6a168d15ce
Elyor <talk0248@gmail.com>
2021-01-28 15:34:43 +0500

    fiftieth time

--- app/src/main/java/com/example/restuarant/ui/waiter/WaiterFragment.kt
+++ app/src/main/java/com/example/restuarant/ui/waiter/WaiterFragment.kt
@@ -9,9 +9,7 @@ import androidx.recyclerview.widget.LinearSnapHelper
 import com.example.restuarant.R
 import com.example.restuarant.databinding.FragmentWaiterBinding
 import com.example.restuarant.model.entities.CategoryData
-import com.example.restuarant.model.entities.OrderSendData
 import com.example.restuarant.model.entities.TableData
-import com.example.restuarant.model.entities.WaiterOrderData
 import com.example.restuarant.presentation.waiter.WaiterPresenter
 import com.example.restuarant.presentation.waiter.WaiterView
 import com.example.restuarant.ui.global.BaseFragment
@@ -22,7 +20,8 @@ import moxy.presenter.ProvidePresenter
 class WaiterFragment : BaseFragment(), WaiterView {
     override val layoutRes: Int = R.layout.fragment_waiter
 
-    private lateinit var binding: FragmentWaiterBinding
+    private var _bn: FragmentWaiterBinding? = null
+    private val bn get() = _bn ?: throw NullPointerException("error")
     private lateinit var itemList: ArrayList<CategoryData>
     private lateinit var tablePageList: ArrayList<TableData>
     private var goodsCategoryAdapter = CategoryItemAdapter()
@@ -41,40 +40,40 @@ class WaiterFragment : BaseFragment(), WaiterView {
     @SuppressLint("ResourceAsColor")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentWaiterBinding.bind(view)
+        _bn = FragmentWaiterBinding.bind(view)
 
-        binding.tablesBtn.setBackgroundResource(R.color.red)
+        bn.tablesBtn.setBackgroundResource(R.color.red)
         val items = loadMenuItems()
         val tableList = loadTableList()
 
-        binding.tablesBtn.setOnClickListener {
+        bn.tablesBtn.setOnClickListener {
             presenter.changeColor()
             btnId = 1
             presenter.showTables()
-            binding.tablesBtn.setBackgroundResource(R.color.red)
+            bn.tablesBtn.setBackgroundResource(R.color.red)
         }
 
-          binding.orderBtn.setOnClickListener {
+        bn.orderBtn.setOnClickListener {
             presenter.changeColor()
             btnId = 2
             presenter.showMenu()
-            binding.orderBtn.setBackgroundResource(R.color.red)
-              if (binding.tableNumber.text != "0") {
-                  binding.tableNumber.text = "0"
-              }
+            bn.orderBtn.setBackgroundResource(R.color.red)
+            if (bn.tableNumber.text != "0") {
+                bn.tableNumber.text = "0"
+            }
         }
 
-        binding.dashboardBtn.setOnClickListener {
+        bn.dashboardBtn.setOnClickListener {
             presenter.changeColor()
             btnId = 3
-            binding.dashboardBtn.setBackgroundResource(R.color.red)
+            bn.dashboardBtn.setBackgroundResource(R.color.red)
         }
 
-        binding.exitBtn.setOnClickListener {
+        bn.exitBtn.setOnClickListener {
             presenter.changeColor()
             btnId = 4
             presenter.onBackPressed()
-            binding.exitBtn.setBackgroundResource(R.color.red)
+            bn.exitBtn.setBackgroundResource(R.color.red)
         }
 
         goodsCategoryAdapter = CategoryItemAdapter()
@@ -88,13 +87,13 @@ class WaiterFragment : BaseFragment(), WaiterView {
             presenter.showMenu()
             presenter.changeColor()
             btnId = 2
-            binding.orderBtn.setBackgroundResource(R.color.red)
-            binding.tableNumber.text = it.id.toString()
+            bn.orderBtn.setBackgroundResource(R.color.red)
+            bn.tableNumber.text = it.id.toString()
         }
         orderAdapter = OrderAdapter()
 //        orderAdapter.submitList()
 
-        binding.btnPrint.setOnClickListener {
+        bn.btnPrint.setOnClickListener {
 //            val waiterOrderData = WaiterOrderData()
 //            val orderSendData = OrderSendData("","","",)
         }
@@ -109,15 +108,15 @@ class WaiterFragment : BaseFragment(), WaiterView {
 
 
         val snapHelper1 = LinearSnapHelper()
-        snapHelper1.attachToRecyclerView(binding.categoryRv)
+        snapHelper1.attachToRecyclerView(bn.categoryRv)
 
-        binding.menuRv.adapter = goodsCategoryAdapter
+        bn.menuRv.adapter = goodsCategoryAdapter
 
-        binding.categoryRv.adapter = categoryAdapter
+        bn.categoryRv.adapter = categoryAdapter
 
-        binding.tablePageRv.adapter = tableAdapter
+        bn.tablePageRv.adapter = tableAdapter
 
-        binding.orderRv.adapter = orderAdapter
+        bn.orderRv.adapter = orderAdapter
 
     }
 
@@ -132,6 +131,7 @@ class WaiterFragment : BaseFragment(), WaiterView {
 
     override fun openErrorDialog(message: String, status: Boolean) {
 
+
     }
 
     override fun openClientCountDialog() {
@@ -143,30 +143,30 @@ class WaiterFragment : BaseFragment(), WaiterView {
     }
 
     override fun showTables() {
-        binding.categoryConstraint.visibility = View.GONE
-        binding.menuRv.visibility = View.GONE
-        binding.tablePageRv.visibility = View.VISIBLE
+        bn.categoryConstraint.visibility = View.GONE
+        bn.menuRv.visibility = View.GONE
+        bn.tablePageRv.visibility = View.VISIBLE
     }
 
     override fun showMenu() {
-        binding.categoryConstraint.visibility = View.VISIBLE
-        binding.menuRv.visibility = View.VISIBLE
-        binding.tablePageRv.visibility = View.GONE
+        bn.categoryConstraint.visibility = View.VISIBLE
+        bn.menuRv.visibility = View.VISIBLE
+        bn.tablePageRv.visibility = View.GONE
     }
 
     override fun changeColor() {
         when (btnId) {
-            1 -> binding.tablesBtn.setBackgroundResource(R.color.green)
-            2 -> binding.orderBtn.setBackgroundResource(R.color.green)
-            3 -> binding.dashboardBtn.setBackgroundResource(R.color.green)
-            4 -> binding.exitBtn.setBackgroundResource(R.color.green)
+            1 -> bn.tablesBtn.setBackgroundResource(R.color.green)
+            2 -> bn.orderBtn.setBackgroundResource(R.color.green)
+            3 -> bn.dashboardBtn.setBackgroundResource(R.color.green)
+            4 -> bn.exitBtn.setBackgroundResource(R.color.green)
         }
     }
 
     private fun loadTableList(): ArrayList<TableData> {
         tablePageList = ArrayList()
         for (i in 0..30) {
-            tablePageList.add(TableData(i + 1, "", "",false,i+1))
+            tablePageList.add(TableData(i + 1, "", "", false, i + 1))
         }
 
         return tablePageList
@@ -193,6 +193,11 @@ class WaiterFragment : BaseFragment(), WaiterView {
 //        return tableList
 //
 //    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _bn = null
+    }
 
 
 }
