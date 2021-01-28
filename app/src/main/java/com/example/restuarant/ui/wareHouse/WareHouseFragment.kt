e98bd79380ae6fe74ac6cae8df40ea766b278c0c
acer <davronbekprogrammer@gmail.com>
2021-01-28 15:33:43 +0500

    ware_house_add_product11hgyguyaaaaaaa

--- app/src/main/java/com/example/restuarant/ui/wareHouse/WareHouseFragment.kt
+++ app/src/main/java/com/example/restuarant/ui/wareHouse/WareHouseFragment.kt
@@ -2,9 +2,11 @@ package com.example.restuarant.ui.wareHouse
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.restuarant.R
 import com.example.restuarant.databinding.FragmentWareHouseBinding
 import com.example.restuarant.extentions.showSnackMessage
+import com.example.restuarant.model.entities.ProductInData
 import com.example.restuarant.presentation.were_house.WareHousePresenter
 import com.example.restuarant.presentation.were_house.WareHouseView
 import com.example.restuarant.ui.global.BaseFragment
@@ -14,6 +16,9 @@ import moxy.presenter.ProvidePresenter
 class WareHouseFragment() : BaseFragment(), WareHouseView {
     override val layoutRes: Int = R.layout.fragment_ware_house
     private lateinit var binding: FragmentWareHouseBinding
+    private lateinit var layoutManager: LinearLayoutManager
+    private lateinit var itemList: ArrayList<ProductInData>
+    private var adapter = WareHouseAdapter()
 
     @InjectPresenter
     lateinit var presenter: WareHousePresenter
@@ -25,12 +30,19 @@ class WareHouseFragment() : BaseFragment(), WareHouseView {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentWareHouseBinding.bind(view)
-
+        layoutManager = LinearLayoutManager(requireContext())
+        loadAdapter()
 
 
         binding.btnLogOut.setOnClickListener {
             presenter.onBackPressed()
         }
+    }
+
+    private fun loadAdapter() {
+        itemList = ArrayList()
+       
+
     }
 
     override fun showMessage(message: String) {
