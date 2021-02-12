191f36dbdf3575088b2aca29adeb2e5229919335
acer <davronbekprogrammer@gmail.com>
2021-02-12 15:56:45 +0500

    ssssssssssssssssssssssssss

--- app/src/main/java/com/example/restuarant/model/entities/ProductData.kt
+++ app/src/main/java/com/example/restuarant/model/entities/ProductData.kt
@@ -2,7 +2,5 @@ package com.example.restuarant.model.entities
 
 data class ProductData(
     val name: String,
-    val count: Double?,
-    val incomePrice: Double?,
-    val sellPrice: Double
+//   val type:
 )
